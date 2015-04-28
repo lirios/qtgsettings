facae8e2080fcbeb23515ddd038dce431e099b1c
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-04-28 23:47:26 +0200

    Qt wrapper for GSettings
    KConfig is giving problems with QtGui types such as QColor, besides that
    it doesn't have change notifications and no schema support.
    It's a 10+ years old configuration system and it shows, mostly designed
    to create configuration user interfaces that look all the same.
    
    Change notifications can be implemented with KDirWatch at the cost
    of adding fam to the dependencies.  On the other hand we already need
    glib for Hawaii (for example NetworkManager support) and Qt is often
    built with the glib event loop support.
    
    With QtConfiguration I attempted to write a nice Qt wrapper with
    several backends, including dconf.
    
    The goal was and still is to provide a Qt alternative to GSettings.
    However I don't have much to finish QtConfiguration, so a GSettings
    wrapper should be enough for now.

---
+++ src/settings/logging.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * This file is part of Hawaii.
+ *
+ * Copyright (C) 2015 Pier Luigi Fiorini
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * $BEGIN_LICENSE:LGPL3$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <QtCore/QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(QGSETTINGS)
+
+#endif // LOGGING_H
+
