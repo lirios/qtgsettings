309b6b0b8db4b318d033c01417069613caef1450
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-11-02 10:09:01 +0100

    Use Fedora for the CI
    KDE Neon on 16.04 is EOL, better migrate to a supported distribution.

--- .travis/build.sh
+++ .travis/build.sh
@@ -14,7 +14,7 @@ travis_end "artifacts"
 travis_start "qbs_setup"
 msg "Setup qbs..."
 qbs setup-toolchains --detect
-qbs setup-qt $(which qmake) travis-qt5
+qbs setup-qt $(which qmake-qt5) travis-qt5
 qbs config profiles.travis-qt5.baseProfile $CC
 travis_end "qbs_setup"
 
@@ -25,7 +25,7 @@ dbus-run-session -- \
 xvfb-run -a -s "-screen 0 800x600x24" \
 qbs -d build -j $(nproc) --all-products profile:travis-qt5 \
     modules.lirideployment.prefix:/usr \
-    modules.lirideployment.libDir:/usr/lib/x86_64-linux-gnu \
-    modules.lirideployment.qmlDir:/usr/lib/x86_64-linux-gnu/qt5/qml \
-    modules.lirideployment.pluginsDir:/usr/lib/x86_64-linux-gnu/qt5/plugins
+    modules.lirideployment.libDir:/usr/lib64 \
+    modules.lirideployment.qmlDir:/usr/lib64/qt5/qml \
+    modules.lirideployment.pluginsDir:/usr/lib64/qt5/plugins
 travis_end "build"
